ff926c2336398f72e3dd15a26ccef2848482a790
Dhawani-Nanda30 <dhawaninanda30@gmail.com>
2026-04-15 20:47:33 +0530

    Update transform.py

--- schema/schema/pipeline/pipeline/transform.py
+++ schema/schema/pipeline/pipeline/transform.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+def transform(df):
+    df = df.drop_duplicates()
+    df = df.dropna()
+    df['event_time'] = pd.to_datetime(df['event_time'])
+    print("Data transformed")
+    return df
